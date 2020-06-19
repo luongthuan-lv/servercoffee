b309d7652f0fee675e109538a262b4096c4128f7
luongthuan-lv <luongthuan2k@gmail.com>
2020-06-19 23:17:45 +0700

    make it better

--- index.js
+++ index.js
@@ -512,8 +512,8 @@ app.get('/sum-invoice', async (req, res) => {
     //
     // console.log(array1.reduce(reducer));
     // res.send(array1.reduce(reducer));
-  
-    let araysum= db.Invoices.distinct('totalValue');
+
+    let araysum= db.invoice.distinct("totalValue");
 
 
     res.send(araysum);
