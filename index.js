0c9abfc57f699fe45d7f1a7ca851a3042d9c13dc
luongthuan-lv <luongthuan2k@gmail.com>
2020-06-22 23:09:55 +0700

    make it better

--- index.js
+++ index.js
@@ -517,7 +517,7 @@ app.get('/count-invoice', async (req, res) => {
                         "$lte": endDate
                     }
             }},
-        {$group: {_id: "$createDate", total: {$sum: "$totalValue"},count: {
+        {$group: {date: "$createDate", total: {$sum: "$totalValue"},count: {
                     $sum: 1
                 }}},
 
